a4abe12050851d807185232b845f934f497eef24
etmarch <etmarch@gmail.com>
2016-11-19 23:48:53 -0800

    harv

--- tasks.harvester.js
+++ tasks.harvester.js
@@ -16,10 +16,10 @@ const Harv = {
 		var home = Game.getObjectById( Memory.firstSpawn );
 		utils.cL( `why dis not working - ${creep} ${home}` );
 		// Commit suicide if creep can't move or carry
-		if ( creep.getActiveBodyparts( MOVE ) == 0 || creep.getActiveBodyparts( CARRY ) == 0 || creep.getActiveBodyparts( WORK ) == 0 ) {
+		/*if ( creep.getActiveBodyparts( MOVE ) == 0 || creep.getActiveBodyparts( CARRY ) == 0 || creep.getActiveBodyparts( WORK ) == 0 ) {
 			utils.cL( `Fareweel brothers, I am useless! ${creep.name}` );
 			creep.suicide();
-		}
+		}*/
 		utils.cL(` ${creep.pos.findClosest( FIND_SOURCES_ACTIVE )} `);
 		creep.moveTo( creep.pos.findClosest( FIND_SOURCES_ACTIVE ) );
 		// Main loop for behaviors
