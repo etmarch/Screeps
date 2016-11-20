33eec04bb7d15460194ad62ce42883a64dd869da
etmarch <etmarch@gmail.com>
2016-11-20 04:03:38 -0800

    not updating...

--- roles.js
+++ roles.js
@@ -10,7 +10,7 @@ module.exports = function () {
 		'builder': { "parts": [ MOVE, MOVE, CARRY, WORK ], "cost": 250, "fighter": false },
 		'guard': { "parts": [ TOUGH, ATTACK, ATTACK, MOVE, MOVE ], "cost": 270, "fighter": true },
 		'ranger': { "parts": [ TOUGH, TOUGH, TOUGH, RANGED_ATTACK, MOVE ], "cost": 240, "fighter": true },
-		'healer': { "parts": [ TOUGH, TOUGH, HEAL, MOVE ], "cost": 270, "fighter": true },
+		//'healer': { "parts": [ TOUGH, TOUGH, HEAL, MOVE ], "cost": 270, "fighter": true },
 		'upgrader': { "parts": [ WORK, CARRY, MOVE, MOVE ], "cost": 250, "fighter": false },
 		
 	};
