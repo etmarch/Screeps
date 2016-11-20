684a86e6e1a452fa7b69d928522834af95ce0ed9
etmarch <etmarch@gmail.com>
2016-11-20 00:18:40 -0800

    harv debugging

--- tasks.harvester.js
+++ tasks.harvester.js
@@ -4,8 +4,8 @@
 var _ = require('lodash');
 var utils = require('utils');
 
-const Harv = {
-	run: function ( creep ) {
+var harv = {
+	//run: function ( creep ) {
 		
 		//utils.cL('Inside the harvester code');
 		// need to get the coordinates of things around
@@ -13,13 +13,13 @@ const Harv = {
 		// closest mineral node
 		// get in range, load up, go back to spawn, repeat
 		
-		var home = Game.getObjectById( Memory.firstSpawn );
+		/*var home = Game.getObjectById( Memory.firstSpawn );
 		utils.cL( `why dis not working - ${creep} ${home}` );
 		// Commit suicide if creep can't move or carry
-		/*if ( creep.getActiveBodyparts( MOVE ) == 0 || creep.getActiveBodyparts( CARRY ) == 0 || creep.getActiveBodyparts( WORK ) == 0 ) {
+		/!*if ( creep.getActiveBodyparts( MOVE ) == 0 || creep.getActiveBodyparts( CARRY ) == 0 || creep.getActiveBodyparts( WORK ) == 0 ) {
 			utils.cL( `Fareweel brothers, I am useless! ${creep.name}` );
 			creep.suicide();
-		}*/
+		}*!/
 		utils.cL(` ${creep.pos.findClosest( FIND_SOURCES_ACTIVE )} `);
 		creep.moveTo( creep.pos.findClosest( FIND_SOURCES_ACTIVE ) );
 		// Main loop for behaviors
@@ -41,10 +41,24 @@ const Harv = {
 					creep.transfer( home, RESOURCE_ENERGY );
 				}
 			}
+		}*/
+		
+		run: function(creep) {
+			if(creep.carry.energy < creep.carryCapacity) {
+				var sources = creep.room.find(FIND_SOURCES);
+				if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(sources[0]);
+				}
+			}
+			else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
+				if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(Game.spawns['Spawn1']);
+				}
+			}
 		}
 		
 		
-	}
+	//}
 };
 
-module.exports = Harv;
+module.exports = harv;
