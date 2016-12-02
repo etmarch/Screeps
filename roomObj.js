068ebf09ed6f33f92af9eb95527864f57cd0c440
etmarch <etmarch@gmail.com>
2016-12-02 04:07:12 -0800

    <git-autocommit>

--- roomObj.js
+++ roomObj.js
@@ -11,6 +11,6 @@ module.exports.roomInfo = function ( roomObj ) {
 	const hostCreepsArr = roomObj.find( FIND_HOSTILE_CREEPS );
 	const myCreepsArr = roomObj.find( FIND_MY_CREEPS );
 	
-	utils.cL( `Totals - Active Sources: ${activeSourceArr.length}, Spawns: ${spawnArr.length}, Hostile Creeps: ${hostCreepsArr.length}, My Creeps: ${myCreepsArr.length}` );
-	utils.cL( `Spawn Energy: ${spawnArr[ 0 ].energy}, Spawning? ${(!spawnArr[ 0 ].spawning ? 'N' : 'Y')}` );
+	/*utils.cL( `Totals - Active Sources: ${activeSourceArr.length}, Spawns: ${spawnArr.length}, Hostile Creeps: ${hostCreepsArr.length}, My Creeps: ${myCreepsArr.length}` );
+	utils.cL( `Spawn Energy: ${spawnArr[ 0 ].energy}, Spawning? ${(!spawnArr[ 0 ].spawning ? 'N' : 'Y')}` );*/
 };
