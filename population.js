1652da5304315bd9cd245fd459beb5192d8fae2a
etmarch <etmarch@gmail.com>
2016-12-08 08:22:44 -0800

    Init Memory Updates

--- population.js
+++ population.js
@@ -65,7 +65,7 @@ const Pop = {
 		});*/
 		
 		for (let i = 0; i < _.size(room.memory.safeSourceIds); i++) {
-			utils.cL(` source Id test:  ${utils.jS(room.memory.safeSourceIds[i])}`);
+			utils.cL(` source Id test:  ${utils.jS(room.memory.safeSourceIds)}`);
 			let sourceCount = _.size(room.memory.safeSourceIds[i]);
 			utils.cL(`source count: ${sourceCount}`);
 			if (sourceCount < 3) {
