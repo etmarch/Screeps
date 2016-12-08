3e0b2dc256b0cf286482ed1ba68e265a46d2c1d3
etmarch <etmarch@gmail.com>
2016-12-08 10:04:46 -0800

    Init Memory Updates

--- tasks.harvester.js
+++ tasks.harvester.js
@@ -18,7 +18,7 @@ var harv = {
 			// get assigned target from memory
 			//utils.cL(creep.memory.assignedSource);
 			let sourceTarget = Game.getObjectById(creep.memory.assignedSource);
-			//utils.cL(utils.jS(sourceTarget));
+			utils.cL(utils.jS(sourceTarget));
 			if(creep.carry.energy < creep.carryCapacity) {
 				//var sources = creep.room.find(FIND_SOURCES);
 				if(creep.harvest(sourceTarget) == ERR_NOT_IN_RANGE) {
