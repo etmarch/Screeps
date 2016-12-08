0dd516f8e9a681e7de418011b90e97ee70efc291
etmarch <etmarch@gmail.com>
2016-12-08 07:32:47 -0800

    Init Memory Updates

--- roles.js
+++ roles.js
@@ -8,7 +8,7 @@ let pop = require('population');
 
 module.exports = function () {
 	return {
-		'harvester': { "parts": [ WORK, CARRY, MOVE, MOVE ], "fighter": false, assignedSource: pop.assignHarvToSource() },
+		'harvester': { "parts": [ WORK, CARRY, MOVE, MOVE ], "fighter": false, "assignedSource": () => pop.assignHarvToSource() },
 		'builder': { "parts": [ MOVE, MOVE, CARRY, WORK ], "fighter": false },
 		'guard': { "parts": [ TOUGH, ATTACK, ATTACK, MOVE, MOVE ], "fighter": true },
 		'ranger': { "parts": [ TOUGH, TOUGH, TOUGH, RANGED_ATTACK, MOVE ],  "fighter": true },
