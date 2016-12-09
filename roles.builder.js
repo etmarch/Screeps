263294a2a21e33ca138bbaeb3b932a2a7768c70c
etmarch <etmarch@gmail.com>
2016-12-09 03:45:08 -0800

    Init Memory Updates

--- roles.builder.js
+++ roles.builder.js
@@ -24,9 +24,11 @@ const builder = {
 			}
 		}
 		else {
-			var sources = creep.room.find( FIND_SOURCES );
-			if ( creep.harvest( sources[ 0 ] ) == ERR_NOT_IN_RANGE ) {
-				creep.moveTo( sources[ 0 ] );
+			//var sources = creep.room.find( FIND_SOURCES );
+			let spawn = Game.getObjectById(Memory.initialSpawnId);
+			if ( creep.withdraw( spawn, RESOURCE_ENERGY ) == ERR_NOT_IN_RANGE ) {
+			//if ( creep.harvest( sources[ 0 ] ) == ERR_NOT_IN_RANGE ) {
+				creep.moveTo( spawn );
 			}
 		}
 	}
