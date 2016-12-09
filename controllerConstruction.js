54299ffe7457f89f531090e2526381e7bf54b12f
etmarch <etmarch@gmail.com>
2016-12-09 05:10:26 -0800

    Init Memory Updates

---
+++ controllerConstruction.js
@@ -0,0 +1,13 @@
+/**
+ * Created by evanmarchand on 12/9/16.
+ */
+
+var _ = require( 'lodash' );
+var utils = require( 'utils' );
+
+
+const controllerConstruction = {
+	
+};
+
+module.exports = controllerConstruction;
