ebf08045363689d353928414f728d6166ca6a780
etmarch <etmarch@gmail.com>
2016-12-14 00:07:52 -0800

    Init Memory Updates

--- memoryController.js
+++ memoryController.js
@@ -41,8 +41,17 @@ const memoryController = {
 		
 		// get room total capacity for storage
 		let totalEnergyCapacity = room.energyCapacityAvailable;
+		const spawnCoords = {
+			x: room.memory.spawnPos.x,
+			y: room.memory.spawnPos.y
+		};
 		
-		
+		const spawn = Game.getObjectById(room.memory.spawnId);
+		// use findPath(fromPos, toPos, [opts]) to map out paths to sources
+		let fastestPath = room.findPath(spawn.pos, room.memory.safeSourceIds['source0'].pos);
+		let fastestPathSerial = room.findPath(spawn.pos, room.memory.safeSourceIds['source0'].pos, {serialize:true});
+		utils.cL(`fastest path: ${fastestPath}`);
+		utils.cL(`fastest path serial: ${fastestPathSerial}`);
 		
 	}
 	
