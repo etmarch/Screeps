0004769d84e4b5559799b8bb4c074ddbf69e8c23
etmarch <etmarch@gmail.com>
2016-12-14 05:47:48 -0800

    Init Memory Updates

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let builder = require( 'roles.builder' );
 var roomController = require( 'roomController' );
 var pop = require( 'population' );
 var memoryController = require('memoryController');
-let log = require('logger');
+var log = require('logger');
 
 const room = _.head( _.values( Game.rooms ) );
 const mainSpawn = room.find( FIND_MY_SPAWNS )[ 0 ];
