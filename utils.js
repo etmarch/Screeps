ea34a2de9e677ed0161b2e3888b2f946f9de75ac
etmarch <etmarch@gmail.com>
2016-12-14 07:32:37 -0800

    Init Memory Updates

--- utils.js
+++ utils.js
@@ -84,7 +84,8 @@ const Utils = {
 		
 		// sort by distance to spawn (so it starts from inside and goes out
 		let testSort = _.map(plainArray, function(value, index, collection){
-			return spawn.pos.getRangeTo(plainArray[index].x, plainArray[index].y);
+			const distance = spawn.pos.getRangeTo(plainArray[index].x, plainArray[index].y);
+			
 		});
 		Utils.cL(`test sort - ${(testSort) }`);
 		Utils.cL(Utils.jS(testSort));
