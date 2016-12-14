9a2ed50d14fdaa75443d1eca0cca534c7e7f4f51
etmarch <etmarch@gmail.com>
2016-12-14 03:50:32 -0800

    Init Memory Updates

--- roomController.js
+++ roomController.js
@@ -24,7 +24,7 @@ const roomController = {
 		//let isAreaClear = _.every(terrainArray, 'terrain', 'plain');
 		//console.log(utils.jS(terrainArray));
 		//utils.cL(isAreaClear);
-		utils.cL(utils.isAreaAllPlains(spawnY-2, spawnX-2, spawnY+2, spawnX+2));
+		//utils.cL(utils.isAreaAllPlains(spawnY-2, spawnX-2, spawnY+2, spawnX+2));
 	},
 	/*
 	*  Should be upgrading controller? or prioritize building....
