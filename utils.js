0fd738f616d9b9fcfaf6819a1606e68749968288
etmarch <etmarch@gmail.com>
2016-12-14 06:58:14 -0800

    Init Memory Updates

--- utils.js
+++ utils.js
@@ -80,7 +80,7 @@ const Utils = {
 		const sourceY = spawn.pos.y, sourceX = spawn.pos.x;
 		const terrainArray = spawn.room.lookForAtArea( LOOK_TERRAIN, sourceY - sizeOffset, sourceX - sizeOffset, sourceY + sizeOffset, sourceX + sizeOffset, { asArray: true } );
 		const plainArray = _.filter( terrainArray, 'terrain', 'plain' );
-		utils.cL(utils.jS(plainArray));
+		Utils.cL(Utils.jS(plainArray));
 		// sort by distance to spawn (so it starts from inside and goes out
 		
 	},
