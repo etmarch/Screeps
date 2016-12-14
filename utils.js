aa91b65079d47add501efbc3c1405efc933b623c
etmarch <etmarch@gmail.com>
2016-12-14 04:06:59 -0800

    Init Memory Updates

--- utils.js
+++ utils.js
@@ -72,7 +72,7 @@ const Utils = {
 	
 	countConstructionInRoom: function ( room ) {
 		const allSites = room.find(FIND_MY_CONSTRUCTION_SITES);
-		Utils.cL(`allSites: ${allSites} --- ${Utils.jS(allSites)}`);
+		//Utils.cL(`allSites: ${allSites} --- ${Utils.jS(allSites)}`);
 		return _.size(allSites);
 	},
 	
