607d257e7fdc5fee7ff34657edee59d516092526
etmarch <etmarch@gmail.com>
2016-12-14 07:42:59 -0800

    Init Memory Updates

--- utils.js
+++ utils.js
@@ -90,7 +90,7 @@ const Utils = {
 		});
 		
 		let sorted = _.sortByOrder(plainArray, ['distance'], ['asc']); // Sorting correctly!
-		let sortedList = _.rest(sorted);
+		let sortedList = _.rest(sorted); // removed location of spawn itself
 		Utils.cL(`test sort - ${(testSort) }`);
 		Utils.cL(Utils.jS(sortedList));
 		
