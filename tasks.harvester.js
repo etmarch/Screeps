fc9ef3d1c2a447495d0cd191980e58a115d62235
etmarch <etmarch@gmail.com>
2016-12-29 09:10:35 -0500

    Init Memory Updates

--- tasks.harvester.js
+++ tasks.harvester.js
@@ -18,7 +18,7 @@ var harv = {
 				let roomMem = creep.room.memory;
 				let sourceArr = roomMem.safeSourceIds;
 				utils.cL(`source array: ${sourceArr}`);
-				for (let i = 0; i < _.size(sourceArr); i++ ) {
+				for (let i = 0; i < _.size(sourceArr); i++ ) { //loop through all sources, stop if find not full one
 					utils.jS(sourceArr[i]);
 				}
 				
