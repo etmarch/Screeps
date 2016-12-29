00eaeacb2d5a8742a8cd4420eb7bd2439f1ba287
etmarch <etmarch@gmail.com>
2016-12-28 23:16:42 -0500

    Init Memory Updates

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var log = require('logger');
 
 const room = _.head( _.values( Game.rooms ) );
 const mainSpawn = room.find( FIND_MY_SPAWNS )[ 0 ];
-console.log(mainSpawn);
+console.log(`Main Spawn: ${mainSpawn} `);
 module.exports.loop = function () {
 	
 	/*if (isNaN(Game.cpu.getUsed() / Game.cpu.limit)) {
