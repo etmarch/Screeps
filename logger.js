ed5bda7cb3a515641d06060dc18c7a60a581a7ee
etmarch <etmarch@gmail.com>
2016-12-28 21:24:51 -0500

    Init Memory Updates

--- logger.js
+++ logger.js
@@ -11,7 +11,7 @@ const log = {
 		if (Game.time % tickInterval === 0) {
 			let eAvail = room.energyAvailable, eCap = room.energyCapacityAvailable;
 			const droppedEnergy = room.find(FIND_DROPPED_ENERGY);
-			utils.cLC(`E: ${eAvail}, cap: ${eCap}, %: ${ ((eAvail/eCap) * 100).toFixed(2) } - dropped: ${droppedEnergy}`);
+			utils.cLC(`E: ${eAvail}, cap: ${eCap}, %: ${ ((eAvail/eCap) * 100).toFixed(2) } - dropped: ${_.count(droppedEnergy)}`);
 		}
 	}
 };
