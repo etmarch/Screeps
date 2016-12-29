309a8e1da51ac665772d4f9ffc975cb90b8a70bc
etmarch <etmarch@gmail.com>
2016-12-29 01:06:31 -0500

    Init Memory Updates

--- main.js
+++ main.js
@@ -12,12 +12,7 @@ var memoryController = require('memoryController');
 var log = require('logger');
 
 
-if (Memory.init === undefined) {
-	initMemory.initMemory();
-}
 
-const room = Game.rooms[Memory.startRoom];
-const mainSpawn = room.find( FIND_MY_SPAWNS )[ 0 ];
 
 module.exports.loop = function () {
 	
@@ -26,6 +21,14 @@ module.exports.loop = function () {
 	} else {
 		utils.cL( `-------  START T:(${Game.time}) %:${((Game.cpu.getUsed() / Game.cpu.limit) * 100).toFixed( 2 ) } lvl:${room.controller.level} --------` );
 	}*/
+	
+	if (Memory.init === undefined) {
+		initMemory.initMemory();
+	}
+	
+	const room = Game.rooms[Memory.startRoom];
+	utils.cL(room);
+	const mainSpawn = room.find( FIND_MY_SPAWNS )[ 0 ];
 	
 	utils.cL(`harvsTotal: ${room.memory.maxHarvsTotal}`);
 	log.roomEnergy(room, 5);
