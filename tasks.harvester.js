6aeea126ff6171f19dbae37cf3f1646e6b7ea383
etmarch <etmarch@gmail.com>
2016-12-29 09:22:33 -0500

    Init Memory Updates

--- tasks.harvester.js
+++ tasks.harvester.js
@@ -17,7 +17,7 @@ var harv = {
 			let sourceArr = roomMem.safeSourceIds;
 			//utils.cL(`source array: ${sourceArr}`);
 			for (let i = 0; i < _.size(sourceArr); i++ ) { //loop through all sources, stop if find not full one
-				(utils.cL(JSON.stringify(sourceArr[i])));
+				(utils.cL(typeof (sourceArr[i])));
 			}
 			
 			//utils.cL(_.size(Game.flags));
