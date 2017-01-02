ed552274c39d88d253582bfc599df7e1d1ab7ccd
etmarch <etmarch@gmail.com>
2017-01-02 06:37:31 -0500

    Init Memory Updates

--- tasks.upgrader.js
+++ tasks.upgrader.js
@@ -15,30 +15,32 @@ var upgrader = {
 		}
 		
 		//utils.cL(` ${creep.pos.findClosestByRange( FIND_SOURCES_ACTIVE )} `);
-		
-		if ( creep.memory.upgrading && creep.carry.energy == 0 ) {
-			creep.memory.upgrading = false;
-			creep.say( 'harvesting' );
-		}
-		if ( !creep.memory.upgrading && creep.carry.energy == creep.carryCapacity ) {
-			creep.memory.upgrading = true;
-			creep.say( 'upgrading' );
-		}
-		
-		if ( creep.memory.upgrading ) {
-			if ( creep.upgradeController( creep.room.controller ) == ERR_NOT_IN_RANGE ) {
-				creep.moveTo( creep.room.controller );
+		if ( utils.countRole( 'harvester' ) < creep.room.memory.maxHarvsTotal ) {
+			//do nothing
+		} else {
+			if ( creep.memory.upgrading && creep.carry.energy == 0 ) {
+				creep.memory.upgrading = false;
+				creep.say( 'harvesting' );
 			}
-		}
-		else {
-			//var sources = creep.room.find( FIND_SOURCES );
-			let spawn = Game.getObjectById(Memory.initialSpawnId);
-			if ( creep.withdraw( spawn, RESOURCE_ENERGY ) == ERR_NOT_IN_RANGE ) {
-				creep.moveTo( spawn );
+			if ( !creep.memory.upgrading && creep.carry.energy == creep.carryCapacity ) {
+				creep.memory.upgrading = true;
+				creep.say( 'upgrading' );
+			}
+			
+			if ( creep.memory.upgrading ) {
+				if ( creep.upgradeController( creep.room.controller ) == ERR_NOT_IN_RANGE ) {
+					creep.moveTo( creep.room.controller );
+				}
+			}
+			else {
+				//var sources = creep.room.find( FIND_SOURCES );
+				let spawn = Game.getObjectById( Memory.initialSpawnId );
+				if ( creep.withdraw( spawn, RESOURCE_ENERGY ) == ERR_NOT_IN_RANGE ) {
+					creep.moveTo( spawn );
+				}
 			}
 		}
 	}
-	
 	
 	//}
 };
