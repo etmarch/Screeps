95fe2f0b98d10c30b9d8151dc63a5362c5c643e2
etmarch <etmarch@gmail.com>
2017-01-02 06:29:42 -0500

    Init Memory Updates

--- roles.builder.js
+++ roles.builder.js
@@ -2,33 +2,39 @@
  * Created by evanmarchand on 11/20/16.
  */
 
+let utils = require( 'utils' );
+
 const builder = {
 	/** @param {Creep} creep **/
 	run: function ( creep ) {
-		
-		if ( creep.memory.building && creep.carry.energy == 0 ) {
-			creep.memory.building = false;
-			creep.say( 'harvesting' );
-		}
-		if ( !creep.memory.building && creep.carry.energy == creep.carryCapacity ) {
-			creep.memory.building = true;
-			creep.say( 'building' );
-		}
-		
-		if ( creep.memory.building ) {
-			var targets = creep.room.find( FIND_CONSTRUCTION_SITES );
-			if ( targets.length ) {
-				if ( creep.build( targets[ 0 ] ) == ERR_NOT_IN_RANGE ) {
-					creep.moveTo( targets[ 0 ] );
+		if ( utils.countRole( 'harvester' ) < creep.room.memory.maxHarvsTotal ) {
+			creep.say( 'Waiting' );
+			utils.cL( `need more harvs before resume building` );
+		} else {
+			if ( creep.memory.building && creep.carry.energy == 0 ) {
+				creep.memory.building = false;
+				creep.say( 'harvesting' );
+			}
+			if ( !creep.memory.building && creep.carry.energy == creep.carryCapacity ) {
+				creep.memory.building = true;
+				creep.say( 'building' );
+			}
+			
+			if ( creep.memory.building ) {
+				var targets = creep.room.find( FIND_CONSTRUCTION_SITES );
+				if ( targets.length ) {
+					if ( creep.build( targets[ 0 ] ) == ERR_NOT_IN_RANGE ) {
+						creep.moveTo( targets[ 0 ] );
+					}
 				}
 			}
-		}
-		else {
-			//var sources = creep.room.find( FIND_SOURCES );
-			let spawn = Game.getObjectById(Memory.initialSpawnId);
-			if ( creep.withdraw( spawn, RESOURCE_ENERGY ) == ERR_NOT_IN_RANGE ) {
-			//if ( creep.harvest( sources[ 0 ] ) == ERR_NOT_IN_RANGE ) {
-				creep.moveTo( spawn );
+			else {
+				//var sources = creep.room.find( FIND_SOURCES );
+				let spawn = Game.getObjectById( Memory.initialSpawnId );
+				if ( creep.withdraw( spawn, RESOURCE_ENERGY ) == ERR_NOT_IN_RANGE ) {
+					//if ( creep.harvest( sources[ 0 ] ) == ERR_NOT_IN_RANGE ) {
+					creep.moveTo( spawn );
+				}
 			}
 		}
 	}
